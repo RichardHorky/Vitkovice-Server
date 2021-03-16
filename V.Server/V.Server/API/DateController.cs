58eddaf5d849fdb051c93474d77b22ade3576c24
RichardHorky <richardho@volny.cz>
2021-03-16 22:11:49 +0100

    controllers

--- V.Server/V.Server/API/DateController.cs
+++ V.Server/V.Server/API/DateController.cs
@@ -12,14 +12,21 @@ namespace V.Server.API
     public class DateController : ControllerBase
     {
         [HttpGet]
-        public ActionResult<long> GetCurrentSeconds()
+        public ActionResult<string> GetCurrentSeconds()
         {
-            TimeZoneInfo cstZone = TimeZoneInfo.FindSystemTimeZoneById("Central Europe Standard Time");
-            DateTime cstTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, cstZone);
-            var year = cstTime.Year;
-            var firstInYear = new DateTime(year, 1, 1);
-            var seconds = (long)Math.Floor((cstTime - firstInYear).TotalSeconds);
-            return Ok(seconds);
+            try
+            {
+                TimeZoneInfo cstZone = TimeZoneInfo.FindSystemTimeZoneById("Central Europe Standard Time");
+                DateTime cstTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, cstZone);
+                var year = cstTime.Year;
+                var firstInYear = new DateTime(year, 1, 1);
+                var seconds = (long)Math.Floor((cstTime - firstInYear).TotalSeconds);
+                return Ok(seconds.ToString());
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.ToString());
+            }
         }
     }
 }
