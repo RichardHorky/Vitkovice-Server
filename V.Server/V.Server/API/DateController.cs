db7f5d7d00cca5f9abf88995603a17f8aaf69c5c
RichardHorky <richardho@volny.cz>
2021-03-18 19:36:15 +0100

    api

---
+++ V.Server/V.Server/API/DateController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace V.Server.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DateController : ControllerBase
+    {
+        public ActionResult<long> Get()
+        {
+            var now = DateTime.Now.AddHours(1);
+            var year = now.Year;
+            var firstInYear = new DateTime(year, 1, 1);
+            var seconds = (long)Math.Floor((now - firstInYear).TotalSeconds);
+            return Ok(seconds);
+        }
+    }
+}
