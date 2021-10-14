4f5b4c4e1ee60f1b6341a79c6ff1609c8e7425a4
RichardHorky <richardho@volny.cz>
2021-10-14 18:26:54 +0200

    datum

--- V.Server/V.Server/API/DateController.cs
+++ V.Server/V.Server/API/DateController.cs
@@ -13,10 +13,22 @@ namespace V.Server.API
     {
         public ActionResult<string> Get()
         {
-            TimeZoneInfo cst = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
             var now = DateTime.Now.AddHours(1);
-            if (cst.IsDaylightSavingTime(now))
-                now = now.AddHours(1);
+            //get last march sunday
+            var marchDays = DateTime.DaysInMonth(now.Year, 3);
+            var lastDayOfMarch = new DateTime(now.Year, 3, marchDays);
+            var marchSpan = -(int)lastDayOfMarch.DayOfWeek;
+            var summerBegin = lastDayOfMarch.AddDays(marchSpan);
+
+            //get last october sunday
+            var octDays = DateTime.DaysInMonth(now.Year, 10);
+            var lastDayOfOct = new DateTime(now.Year, 10, octDays);
+            var octSpan = -(int)lastDayOfOct.DayOfWeek;
+            var summerEnd = lastDayOfOct.AddDays(octSpan);
+
+            if (now >= summerBegin && now < summerEnd)
+                now = now.AddDays(1);
+
             var year = now.Year;
             var firstInYear = new DateTime(year, 1, 1);
             var seconds = (long)Math.Floor((now - firstInYear).TotalSeconds);
