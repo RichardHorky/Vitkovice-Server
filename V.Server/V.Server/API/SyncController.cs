61d65d35603fe2b067f6fba530118dc6a884b72e
RichardHorky <richardho@volny.cz>
2021-11-22 11:02:25 +0100

    pokus

--- V.Server/V.Server/API/SyncController.cs
+++ V.Server/V.Server/API/SyncController.cs
@@ -125,7 +125,7 @@ namespace V.Server.API
             {
                 var fnRecovery = _dataStorage.GetData<TransferData.FnItems>("FnRecovery");
                 if (fnRecovery == null)
-                    return Ok("{}");
+                    return Ok("{none}");
 
                 var items = new List<string>();
                 items.Add(((byte)fnRecovery.GetState(TransferData.ButtonPressEnum.Termostat1)).ToString());
