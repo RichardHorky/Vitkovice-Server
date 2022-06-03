3ef7c8f1e404d8bc14453229c4f144250788bad4
RichardHorky <richardho@volny.cz>
2022-06-03 15:55:49 +0200

    DateController

--- V.Server/V.Server/API/DateController.cs
+++ V.Server/V.Server/API/DateController.cs
@@ -20,24 +20,6 @@ namespace V.Server.API
 
         public ActionResult<string> Get()
         {
-            var now = DateTime.Now.AddHours(1);
-            //get last march sunday
-            var marchDays = DateTime.DaysInMonth(now.Year, 3);
-            var lastDayOfMarch = new DateTime(now.Year, 3, marchDays);
-            var marchSpan = -(int)lastDayOfMarch.DayOfWeek;
-            var summerBegin = lastDayOfMarch.AddDays(marchSpan);
-
-            //get last october sunday
-            var octDays = DateTime.DaysInMonth(now.Year, 10);
-            var lastDayOfOct = new DateTime(now.Year, 10, octDays);
-            var octSpan = -(int)lastDayOfOct.DayOfWeek;
-            var summerEnd = lastDayOfOct.AddDays(octSpan);
-
-            if (now >= summerBegin && now < summerEnd)
-                now = now.AddHours(1);
-
-            var year = now.Year;
-            var firstInYear = new DateTime(year, 1, 1);
             var seconds = _dateHelper.GetSeconds();
             return Ok($"{{{seconds}}}");
         }
